6cb365628a39a23edc2bc737285dd2750b1a4662
g <1229081771@qq.com>
2020-06-09 14:55:18 +0800

    first commit

---
+++ src/components/login.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1 @click="show()">这是登录页面</h1>
+    </div>
+</template>
+<style lang="scss" coped>
+
+</style>
+<script>
+    export default {
+        data:function(){
+            return {}
+        },
+        methods:{
+            show(){
+                console.log(this.$axios)
+
+                this.$axios.get("../a.json").then((response1)=>{
+                      console.log(response1.data.name)
+                })
+            }
+        }
+    }
+</script>
---
+++ src/components/register.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>这是注册页面</h1>
+    </div>
+</template>
+<style lang="scss" coped>
+
+</style>
+<script>
+export default {
+    data:function(){
+        return {}
+    },
+    methods:{}
+}
+</script>
+
